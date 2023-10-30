9ed6db42348a549cd93ca1a7d4279c811b71d478
ivasiucd <ivasiucdarina@gmail.com>
2023-10-30 14:30:35 +0200

    JS

---
+++ script.js
@@ -0,0 +1,37 @@
+/* HW 1
+
+let country = 'Italy';
+console.log(country);
+let continent = 'Europe';
+console.log(continent);
+let population = 59.1;
+console.log(population);
+let isIsland = false;
+console.log(isIsland);
+let countryDescription = 'The ${coutry} is located in ${continent}, ${population} million people live there.';
+console.log(countryDescription); */
+
+/* HW 2
+
+let weightSarah = 65;
+let weightBob = 93;
+let heightSarah = 1.75;
+let heightBob = 1.89;
+
+let sarahBMI = weightSarah / heightSarah ** 2;
+console.log(sarahBMI);
+if (sarahBMI < 18.5) {
+            console.log('Sarah is underweight.')
+}
+else if (sarahBMI >= 18.5 && sarahBMI <=25); {
+        console.log('Sarah's weight is normal.')
+}
+
+let bobBMI = weightBob / heightBob ** 2;
+console.log(bobBMI);
+if (bobBMI < 18.5 || bobBMI > 25) {
+      console.log('Bob is underweight or overweight.')
+}
+else {
+      console.log('Bob's weught is normal.')
+}*/
